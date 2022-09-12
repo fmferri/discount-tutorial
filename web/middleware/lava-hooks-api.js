7d2ff15c15465c12365791abc49dfae6e577b4c0
m.di.giovambattista <m.di.giovambattista@accenture.com>
2022-09-12 14:04:09 +0200

    retrieve customer with graphQL

--- web/middleware/lava-hooks-api.js
+++ web/middleware/lava-hooks-api.js
@@ -5,14 +5,28 @@
   This information is retrieved from the Authorization header, which is decoded from the request.
   The authorization header is added by App Bridge in the frontend code.
 */
-
-import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
+import axios from "axios";
+import { request, gql, GraphQLClient } from "graphql-request";
 
 export default function applyLavaHooksApiEndpoints(app) {
-  app.post("/api/lv/cartupdate", cors(), async (req, res) => {
+  app.post("/api/lv/cartupdate", async (req, res) => {
     try {
       console.log("WEBOOK REQUEST CARTUPDATE ARRIVATA");
-      console.log(req.body);
+      console.log("BODY", req.body);
+      // console.log("HEADER", req.headers);
+      // console.log("PARAMS", req.params);
+      const customer = await getUserInfo(req.body.id)
+        .then((data) => {
+          console.log("SUCCESS CUSTOMER", JSON.stringify(data, undefined, 2));
+          return data;
+        })
+        .catch((err) => {
+          console.log("ERROR", err);
+        });
+      console.log("RETRIEVED CUSTOMER", customer);
+      // updateUserInfo(req.body.id);
       const discountResponse = await checkForDiscount(req.body);
       res.header("Access-Control-Allow-Origin");
       res.send(discountResponse);
@@ -23,6 +37,85 @@ export default function applyLavaHooksApiEndpoints(app) {
     }
   });
 }
+
+const getUserInfo = async (id) => {
+  console.log("TESTING getUserInfo");
+  id = "gid://shopify/Customer/" + id;
+  const endpoint =
+    "https://mdgb-dev-store.myshopify.com/admin/api/2022-07/graphql.json";
+  const variables = {};
+  const query =
+    gql`
+    {
+      customer(id: "` +
+    id +
+    `") {
+      createdAt
+      displayName
+      email
+      firstName
+      id
+      lastName
+      lifetimeDuration
+      note
+      numberOfOrders
+      state
+      tags
+      updatedAt
+      }
+    }
+  `;
+  console.log("QUERY", query);
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      "X-Shopify-Access-Token": "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",
+    },
+  });
+  return client.request(query, variables);
+};
+
+const updateUserInfo = async (id) => {
+  console.log("TESTING updateUserInfo");
+  id = "gid://shopify/Customer/" + id;
+  const endpoint =
+    "https://mdgb-dev-store.myshopify.com/admin/api/2022-07/graphql.json";
+
+  const mutation = gql`
+    mutation customerUpdate($input: CustomerInput) {
+      customerUpdate(input: $input) {
+        customer {
+          tags
+          id
+        }
+        userErrors {
+          field
+          message
+        }
+      }
+    }
+  `;
+  console.log("mutation", mutation);
+  const variables = {
+    input: {
+      id: id,
+      tags: ["SILVER-50"],
+    },
+  };
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      "X-Shopify-Access-Token": "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",
+    },
+  });
+  client
+    .request(mutation, variables)
+    .then((data) => {
+      console.log("CUSTOMER Updated", JSON.stringify(data, undefined, 2));
+      return data;
+    })
+    .catch((err) => {
+      console.log("ERROR", err);
+    });
+};
 
 async function checkForDiscount(data) {
   const response = { statusDiscount: "OK" };
