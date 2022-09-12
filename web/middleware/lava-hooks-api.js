8efa68927c7f1bef593ddbc806cedfe31a798968
m.di.giovambattista <m.di.giovambattista@accenture.com>
2022-09-12 22:08:13 +0200

    update customer tags based on API response

--- web/middleware/lava-hooks-api.js
+++ web/middleware/lava-hooks-api.js
@@ -5,7 +5,7 @@
   This information is retrieved from the Authorization header, which is decoded from the request.
   The authorization header is added by App Bridge in the frontend code.
 */
-import * as dotenv from "dotenv";
+import dotenv from "dotenv";
 dotenv.config();
 import axios from "axios";
 import { request, gql, GraphQLClient } from "graphql-request";
@@ -17,16 +17,25 @@ export default function applyLavaHooksApiEndpoints(app) {
       console.log("BODY", req.body);
       // console.log("HEADER", req.headers);
       // console.log("PARAMS", req.params);
-      const customer = await getUserInfo(req.body.id)
+      const customer = getCustomerInfo(req.body.id)
         .then((data) => {
           console.log("SUCCESS CUSTOMER", JSON.stringify(data, undefined, 2));
+          const updatedCustomer = updateCustomerInfo(data).then(
+            (updatedCustomer) => {
+              console.log("UPDATED CUSTOMER", updatedCustomer);
+              const updatedCustomer2 = getCustomerInfo(req.body.id).then(
+                (updatedCustomer2) => {
+                  console.log("UPDATED CUSTOMER2", updatedCustomer2);
+                }
+              );
+            }
+          );
           return data;
         })
         .catch((err) => {
           console.log("ERROR", err);
         });
-      console.log("RETRIEVED CUSTOMER", customer);
-      // updateUserInfo(req.body.id);
+
       const discountResponse = await checkForDiscount(req.body);
       res.header("Access-Control-Allow-Origin");
       res.send(discountResponse);
@@ -38,7 +47,7 @@ export default function applyLavaHooksApiEndpoints(app) {
   });
 }
 
-const getUserInfo = async (id) => {
+const getCustomerInfo = async (id) => {
   console.log("TESTING getUserInfo");
   id = "gid://shopify/Customer/" + id;
   const endpoint =
@@ -50,42 +59,46 @@ const getUserInfo = async (id) => {
       customer(id: "` +
     id +
     `") {
-      createdAt
-      displayName
-      email
-      firstName
-      id
-      lastName
-      lifetimeDuration
-      note
-      numberOfOrders
-      state
-      tags
-      updatedAt
+      	id
+        firstName
+        lastName
+        acceptsMarketing
+        email
+        phone
+        createdAt
+        updatedAt
+        note
+        verifiedEmail
+        validEmailAddress
+        tags
+        lifetimeDuration
       }
     }
   `;
   console.log("QUERY", query);
   const client = new GraphQLClient(endpoint, {
     headers: {
-      "X-Shopify-Access-Token": "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",
+      "X-Shopify-Access-Token": process.env.ADMIN_API_ACCESS_TOKEN,
     },
   });
   return client.request(query, variables);
 };
 
-const updateUserInfo = async (id) => {
-  console.log("TESTING updateUserInfo");
-  id = "gid://shopify/Customer/" + id;
+const updateCustomerInfo = async (customer) => {
+  customer = JSON.parse(JSON.stringify(customer)).customer;
+  customer.tags = "SILVER-50";
+  console.log("TESTING updateCustomerInfo", customer);
+  console.log("CUSTOMER ID", customer.id);
+  // id = customer.customer.id;
   const endpoint =
     "https://mdgb-dev-store.myshopify.com/admin/api/2022-07/graphql.json";
 
   const mutation = gql`
-    mutation customerUpdate($input: CustomerInput) {
+    mutation customerUpdate($input: CustomerInput!) {
       customerUpdate(input: $input) {
         customer {
-          tags
           id
+          tags
         }
         userErrors {
           field
@@ -97,16 +110,16 @@ const updateUserInfo = async (id) => {
   console.log("mutation", mutation);
   const variables = {
     input: {
-      id: id,
-      tags: ["SILVER-50"],
+      id: customer.id,
+      tags: customer.tags,
     },
   };
   const client = new GraphQLClient(endpoint, {
     headers: {
-      "X-Shopify-Access-Token": "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",
+      "X-Shopify-Access-Token": process.env.ADMIN_API_ACCESS_TOKEN,
     },
   });
-  client
+  return client
     .request(mutation, variables)
     .then((data) => {
       console.log("CUSTOMER Updated", JSON.stringify(data, undefined, 2));
