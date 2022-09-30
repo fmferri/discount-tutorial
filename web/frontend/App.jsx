dda340ddd25eb72694d6f66604f9352484839b18
Your Name <fabio.ferri@catenate.com>
2022-09-30 16:18:54 +0200

    corretto file

--- web/frontend/App.jsx
+++ web/frontend/App.jsx
@@ -28,10 +28,6 @@ export default function App() {
                   },
                 ]}
               />
-<<<<<<< Updated upstream
-              
-=======
->>>>>>> Stashed changes
               <Routes pages={pages} />
             </QueryProvider>
           </DiscountProvider>
